f52d01f868fb5299b07cf5e15bee266bce68f7fa
KevinP <kpiskorowski81@gmail.com>
2025-04-21 20:18:43 -0400

    Deploy to Github Pages

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from pagegenerate import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_template = "./template.html"
 dir_path_content = "./content"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -19,7 +25,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, dir_path_template, dir_path_public)
+    generate_pages_recursive(dir_path_content, dir_path_template, dir_path_public, basepath)
     
 if __name__ =="__main__":
     main()
